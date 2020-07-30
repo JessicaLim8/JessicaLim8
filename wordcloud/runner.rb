4226e24a29d8d79d616f40a410fe460d5ec785a0
JessicaLim8 <jessicalim@bell.net>
2020-07-30 13:59:09 -0400

    fix file writing bug

--- wordcloud/runner.rb
+++ wordcloud/runner.rb
@@ -67,7 +67,7 @@ class Runner
     # Check for spaces
     word = word.gsub("_", " ")
     # Add word to list
-    open('wordcloud/wordlist.txt', 'a') do |f|
+    File.open('wordcloud/wordlist.txt', 'a') do |f|
       f.puts word
     end
     word
