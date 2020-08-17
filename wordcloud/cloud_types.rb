17fbcf45d50fa5369ef833e132af17207407deb5
JessicaLim8 <jessicalim@bell.net>
2020-08-17 15:07:29 -0400

    Add new cloud data

--- wordcloud/cloud_types.rb
+++ wordcloud/cloud_types.rb
@@ -1,4 +1,4 @@
 module CloudTypes
-  CLOUDLABELS = ["quarantine", "food"]
-  CLOUDPROMPTS = ["Favourite Quarantine Passtime? :lock: :tennis: :video_game:", "Favourite Food? :yum: :pizza: :cookie:"]
+  CLOUDLABELS = ["quarantine", "food", "company"]
+  CLOUDPROMPTS = ["Favourite Quarantine Passtime? :lock: :tennis: :video_game:", "Favourite Food? :yum: :pizza: :cookie:", "Favourite/Dream Company? :money_with_wings: :star2: :chart_with_upwards_trend:"]
 end
