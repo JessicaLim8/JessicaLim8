c224e41fb890beb866e6d4961864dc4db36b29e2
JessicaLim8 <jessicalim@bell.net>
2020-09-28 19:55:51 -0400

    Revert "New podcast cloud"
    This reverts commit a6f42bf329a0949797316deee3179902389ea81d.

--- wordcloud/cloud_types.rb
+++ wordcloud/cloud_types.rb
@@ -1,14 +1,4 @@
 module CloudTypes
-  CLOUDLABELS = [
-    "quarantine",
-    "food",
-    "company",
-    "podcast"
-  ]
-  CLOUDPROMPTS = [
-    "Favourite Quarantine Passtime? :lock: :tennis: :video_game:",
-    "Favourite Food? :yum: :pizza: :cookie:",
-    "Favourite/Dream Company? :money_with_wings: :star2: :chart_with_upwards_trend:",
-    "Favourite Podcast? :microphone: :headphones: :notes:"
-  ]
+  CLOUDLABELS = ["quarantine", "food", "company"]
+  CLOUDPROMPTS = ["Favourite Quarantine Passtime? :lock: :tennis: :video_game:", "Favourite Food? :yum: :pizza: :cookie:", "Favourite/Dream Company? :money_with_wings: :star2: :chart_with_upwards_trend:"]
 end
