a6f42bf329a0949797316deee3179902389ea81d
JessicaLim8 <jessicalim@bell.net>
2020-09-28 19:10:11 -0400

    New podcast cloud

--- wordcloud/cloud_types.rb
+++ wordcloud/cloud_types.rb
@@ -1,4 +1,14 @@
 module CloudTypes
-  CLOUDLABELS = ["quarantine", "food", "company"]
-  CLOUDPROMPTS = ["Favourite Quarantine Passtime? :lock: :tennis: :video_game:", "Favourite Food? :yum: :pizza: :cookie:", "Favourite/Dream Company? :money_with_wings: :star2: :chart_with_upwards_trend:"]
+  CLOUDLABELS = [
+    "quarantine",
+    "food",
+    "company",
+    "podcast"
+  ]
+  CLOUDPROMPTS = [
+    "Favourite Quarantine Passtime? :lock: :tennis: :video_game:",
+    "Favourite Food? :yum: :pizza: :cookie:",
+    "Favourite/Dream Company? :money_with_wings: :star2: :chart_with_upwards_trend:",
+    "Favourite Podcast? :microphone: :headphones: :notes:"
+  ]
 end
