9a9fffc76ae3449e7ed30e290444b7a4128d100b
JessicaLim8 <jessicalim@bell.net>
2020-09-28 20:23:53 -0400

    add new podcast cloud

--- wordcloud/cloud_types.rb
+++ wordcloud/cloud_types.rb
@@ -9,6 +9,6 @@ module CloudTypes
     "Favourite Quarantine Passtime? :lock: :tennis: :video_game:",
     "Favourite Food? :yum: :pizza: :cookie:",
     "Favourite/Dream Company? :money_with_wings: :star2: :chart_with_upwards_trend:",
-    "Favourite Podcast? :microphone: :headphones: :notes:"
+    "What podcast are you currently listening to? :microphone: :headphones: :notes:"
   ]
 end
